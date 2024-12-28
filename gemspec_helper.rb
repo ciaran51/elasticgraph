634eae70c87c2dfac9e09dbb0dcfb987e0c2fb71
Myron Marston <myron@squareup.com>
2024-12-27 18:12:44 -0800

    Validate our gemspecs using an RSpec test.
    This replaces the validation we were doing in `gemspec_helper.rb`.
    The `gemspec_helper.rb` file doesn't appear to be compatible with
    dependabot, and we're going to stop using it in a later PR.

--- gemspec_helper.rb
+++ gemspec_helper.rb
@@ -12,7 +12,6 @@ module ElasticGraphGemspecHelper
   # Helper methor for defining a gemspec for an elasticgraph gem.
   def self.define_elasticgraph_gem(gemspec_file:, category:)
     gem_dir = ::File.expand_path(::File.dirname(gemspec_file))
-    validate_gem(gem_dir)
 
     ::Gem::Specification.new do |spec|
       spec.name = ::File.basename(gemspec_file, ".gemspec")
@@ -48,66 +47,6 @@ module ElasticGraphGemspecHelper
       spec.required_ruby_version = "~> 3.2"
 
       yield spec, ElasticGraph::VERSION
-
-      if (symlink_files = spec.files.select { |f| ::File.exist?(f) && ::File.ftype(f) == "link" }).any?
-        raise "#{symlink_files.size} file(s) of the `#{spec.name}` gem are symlinks, but " \
-          "symlinks do not work correctly when the gem is packaged. Symlink files: #{symlink_files.inspect}"
-      end
     end
-  end
-
-  def self.validate_gem(gem_dir)
-    gem_warnings = validate_symlinked_file(::File.join(gem_dir, ".yardopts"))
-
-    gem_issues = []
-    gem_issues.concat(validate_symlinked_file(::File.join(gem_dir, "Gemfile")))
-    gem_issues.concat(validate_symlinked_file(::File.join(gem_dir, ".rspec")))
-    gem_issues.concat(validate_license(gem_dir))
-
-    unless gem_warnings.empty?
-      warn "WARNING: Gem #{::File.basename(gem_dir)} has the following issues:\n\n" + gem_warnings.join("\n")
-    end
-
-    return if gem_issues.empty?
-
-    abort "Gem #{::File.basename(gem_dir)} has the following issues:\n\n" + gem_issues.join("\n")
-  end
-
-  def self.validate_symlinked_file(file)
-    gem_issues = []
-
-    if ::File.exist?(file)
-      if ::File.ftype(file) != "link"
-        gem_issues << "`#{file}` must be a symlink."
-      end
-    else
-      gem_issues << "`#{file}` is missing."
-    end
-
-    gem_issues
-  end
-
-  def self.validate_license(gem_dir)
-    gem_issues = []
-
-    file = ::File.join(gem_dir, "LICENSE.txt")
-    if ::File.exist?(file)
-      if ::File.ftype(file) == "link"
-        gem_issues << "`#{file}` must not be a symlink."
-      end
-
-      contents = ::File.read(file)
-      unless contents.include?("MIT License")
-        gem_issues << "`#{file}` must contain 'MIT License'."
-      end
-
-      unless contents.include?("Copyright (c) 2024 Block, Inc.")
-        gem_issues << "`#{file}` must contain Block copyright notice."
-      end
-    else
-      gem_issues << "`#{file}` is missing."
-    end
-
-    gem_issues
   end
 end
