ff16bca3fb36a8b64e8c47a448451c2638228e0d
Myron Marston <myron@squareup.com>
2025-02-23 19:46:05 -0800

    Symlink site artists schema from `elasticgraph new` template.
    This reduces duplication, and gives us a strong gurantee that the
    example queries on the ElasticGraph website work against the project
    template schema. This is important because we plan to include a call
    to action on the Query API documentation pages to encourage site
    visitors to try out the examples locally by booting the demo app
    using our docker image.

--- config/site/examples/music/schema.rb
+++ config/site/examples/music/schema.rb
@@ -6,69 +6,12 @@
 #
 # frozen_string_literal: true
 
+require "pathname"
+
 ElasticGraph.define_schema do |schema|
   schema.json_schema_version 1
+end
 
-  schema.object_type "Artist" do |t|
-    t.field "id", "ID"
-    t.field "name", "String"
-    t.field "lifetimeSales", "JsonSafeLong"
-    t.field "bio", "ArtistBio"
-
-    t.field "albums", "[Album!]!" do |f|
-      f.mapping type: "nested"
-    end
-
-    t.field "tours", "[Tour!]!" do |f|
-      f.mapping type: "nested"
-    end
-
-    t.index "artists"
-  end
-
-  schema.object_type "ArtistBio" do |t|
-    t.field "yearFormed", "Int"
-    t.field "homeCountry", "String"
-    t.field "description", "String" do |f|
-      f.mapping type: "text"
-    end
-  end
-
-  schema.object_type "Album" do |t|
-    t.field "name", "String"
-    t.field "releasedOn", "Date"
-    t.field "soldUnits", "Int"
-    t.field "tracks", "[AlbumTrack!]!" do |f|
-      f.mapping type: "nested"
-    end
-  end
-
-  schema.object_type "AlbumTrack" do |t|
-    t.field "name", "String"
-    t.field "trackNumber", "Int"
-    t.field "lengthInSeconds", "Int"
-  end
-
-  schema.object_type "Tour" do |t|
-    t.field "name", "String"
-    t.field "shows", "[Show!]!" do |f|
-      f.mapping type: "nested"
-    end
-  end
-
-  schema.object_type "Show" do |t|
-    t.relates_to_one "venue", "Venue", via: "venueId", dir: :out
-    t.field "attendance", "Int"
-    t.field "startedAt", "DateTime"
-  end
-
-  schema.object_type "Venue" do |t|
-    t.field "id", "ID"
-    t.field "name", "String"
-    t.field "location", "GeoLocation"
-    t.field "capacity", "Int"
-    t.relates_to_many "featuredArtists", "Artist", via: "tours.shows.venueId", dir: :in, singular: "featuredArtist"
-
-    t.index "venues"
-  end
+Dir["#{__dir__}/schema/**/*.rb"].each do |schema_def_file|
+  load Pathname(schema_def_file).expand_path
 end
