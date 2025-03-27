16cbf4f799ae7f4f4b63f6c695d470b2c714d00f
Myron Marston <myron@squareup.com>
2025-03-26 18:17:17 -0700

    Generate English bios in our artist factories.
    Previously, we generated latin lorem text. This is more realistic
    (and pretty funny to read!) and it allows more of our example
    queries to find matches.

--- elasticgraph/lib/elastic_graph/project_template/lib/app_name/factories.rb
+++ elasticgraph/lib/elastic_graph/project_template/lib/app_name/factories.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
       albums.map { |a| a.fetch(:soldUnits) }.sum
     end
 
-    bio { build(:artist_bio) }
+    bio { build(:artist_bio, artistName: name) }
 
     genres do
       # Available genres from the MusicGenre enum
@@ -49,7 +49,67 @@ FactoryBot.define do
     __typename { "ArtistBio" }
     yearFormed { Faker::Number.between(from: 1900, to: 2025) }
     homeCountry { Faker::Address.country }
-    description { Faker::Lorem.paragraphs(number: 3).join("\n\n") }
+    description do
+      # Generate a realistic artist bio by combining multiple sentences
+      year = yearFormed
+
+      # Common adjectives for music descriptions
+      adjectives = [
+        "innovative", "groundbreaking", "distinctive", "masterful", "energetic",
+        "soulful", "dynamic", "melodic", "haunting", "powerful", "ethereal",
+        "raw", "polished", "intricate", "atmospheric", "intense", "virtuosic"
+      ].shuffle
+
+      # Musical elements for variety
+      musical_elements = [
+        "harmonies", "compositions", "arrangements", "rhythms", "melodies",
+        "songwriting", "instrumentation", "productions", "performances", "vocals"
+      ].shuffle
+
+      genres = Faker::Base.sample(Faker::Base.translate("faker.music.genres"), 20)
+      instruments = Faker::Base.sample(Faker::Base.translate("faker.music.instruments"), 20).map(&:downcase)
+
+      # Origin story variants
+      origin = [
+        "#{artistName} was formed in #{year} in #{homeCountry}.",
+        "Emerging from #{homeCountry}'s #{genres.pop.downcase} scene in #{year}, #{artistName} began their musical journey.",
+        "Founded in #{homeCountry} in #{year}, #{artistName} started as a collective of #{genres.pop.downcase} musicians.",
+        "The story of #{artistName} began in #{year} when a group of musicians in #{homeCountry} came together."
+      ].sample
+
+      # Musical style variants
+      style = [
+        "Drawing inspiration from #{genres.pop} and #{genres.pop}, they developed a sound featuring #{instruments.pop}, #{instruments.pop}, #{instruments.pop}, and #{instruments.pop}.",
+        "Their #{adjectives.pop} style blends elements of #{genres.pop} with #{genres.pop}, creating a unique musical identity centered on their #{adjectives.pop} #{instruments.pop} and #{instruments.pop} counterpoint.",
+        "Known for their #{adjectives.pop} #{musical_elements.pop}, they combine #{instruments.pop} and #{instruments.pop} with #{adjectives.pop} #{instruments.pop} and #{adjectives.pop} #{instruments.pop} arrangements.",
+        "Their music explores the intersection of #{instruments.pop} #{genres.pop} and #{instruments.pop} #{genres.pop}, highlighted by #{adjectives.pop} #{musical_elements.pop}."
+      ].sample
+
+      # Career highlight variants
+      highlight_year = year + rand(2..5)
+      highlight = [
+        "Their breakthrough came with '#{Faker::Music.album}' in #{highlight_year}, which showcased their #{adjectives.pop} style.",
+        "The release of '#{Faker::Music.album}' in #{highlight_year} marked a turning point, earning critical acclaim for its #{adjectives.pop} #{musical_elements.pop}.",
+        "#{highlight_year} saw the release of their defining work '#{Faker::Music.album}', which demonstrated their #{adjectives.pop} approach to #{musical_elements.pop}.",
+        "With the #{adjectives.pop} '#{Faker::Music.album}' in #{highlight_year}, they established themselves as pioneers in #{genres.pop.downcase}."
+      ].sample
+
+      # Legacy/impact variants
+      legacy = [
+        "They continue to influence the music scene with their #{adjectives.pop} approach to #{musical_elements.pop}.",
+        "Their #{adjectives.pop} contributions to #{genres.pop.downcase} have left a lasting impact on the genre.",
+        "To this day, their #{adjectives.pop} #{musical_elements.pop} continue to inspire new generations of musicians.",
+        "They remain celebrated for their #{adjectives.pop} live performances and #{adjectives.pop} #{musical_elements.pop}.",
+        "Their influence on #{genres.pop.downcase} endures through their #{adjectives.pop} body of work.",
+        "Artists today still draw inspiration from their #{adjectives.pop} approach to #{musical_elements.pop}."
+      ].sample
+
+      [origin, style, highlight, legacy].join(" ")
+    end
+
+    transient do
+      artistName { raise "`artistName` must be provided." }
+    end
   end
 
   factory :album, parent: :hash_base do
