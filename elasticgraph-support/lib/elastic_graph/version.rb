e901384489abc72b05c7bbbb0ccd34cff9ccf566
Myron Marston <myron@squareup.com>
2025-04-28 21:40:59 -0500

    Bump version to 1.0.0.pre.
    The next release will be 1.0.0. This allows us to cleanup a hack
    in `config/release/Rakefile` now that we have a more conventional
    version number (e.g. 3 parts + `.pre` instead of 4 parts + `.pre`).

--- elasticgraph-support/lib/elastic_graph/version.rb
+++ elasticgraph-support/lib/elastic_graph/version.rb
@@ -8,7 +8,7 @@
 
 module ElasticGraph
   # The version of all ElasticGraph gems.
-  VERSION = "0.19.2.2.pre"
+  VERSION = "1.0.0.pre"
 
   # Steep weirdly expects this here...
   # @dynamic self.define_schema
